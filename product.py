e289c075a9a67c46f1e0febe0eb166aedb3186bc
Marco Chen <kojunjunla@outlook.com>
2022-01-08 16:39:27 +0800

    fouorth

--- product.py
+++ product.py
@@ -1,11 +1,17 @@
+import os
+
 data=[]
-with open('product.csv','r',encoding='utf-8') as f:
-    for i in f:
-        if '商品,價格' in i:
-            continue
-        a,b = i.strip().split(',')
-        _=[a,b]
-        data.append(_)
+if os.path.isfile('product.csv'):
+    print('找到檔案')
+    with open('product.csv','r',encoding='utf-8') as f:
+        for i in f:
+            if '商品,價格' in i:
+                continue
+            a,b = i.strip().split(',')
+            _=[a,b]
+            data.append(_)
+else:
+    print('找不到檔案')
 
 while True:
     name = input('請輸入商品: ')
@@ -16,11 +22,16 @@ while True:
     data.append(p)
 print(data)
 
-# with open('product.txt','a') as f:
-#     for product in data:
-#         f.write(product[0] + ',' + str(product[1]) + '\n')
-
 with open('product.csv','w',encoding='utf-8') as f:
     f.write('商品,價格\n')
     for product in data:
         f.write(product[0] + ',' + str(product[1]) + '\n')
+
+
+
+
+# with open('product.txt','a') as f:
+#     for product in data:
+#         f.write(product[0] + ',' + str(product[1]) + '\n')
+
+
