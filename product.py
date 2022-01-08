ad4a7a8a4188905898e7c25a0c814ac3edf59022
Marco Chen <kojunjunla@outlook.com>
2022-01-08 15:23:14 +0800

    update

--- product.py
+++ product.py
@@ -1,12 +1,20 @@
-product = []
+products = []
 while True:
     name = input('請輸入商品: ')
     if name == 'q':
         break
     price = eval(input('請輸入商品價格: '))
     p = [name,price]
-    product.append(p)
-print(product)
+    products.append(p)
+print(products)
 
-for item in product:
+for item in products:
     print(item)
+
+# with open('product.txt','a') as f:
+#     for product in products:
+#         f.write(product[0] + ',' + str(product[1]) + '\n')
+
+with open('product.csv','a') as f:
+    for product in products:
+        f.write(product[0] + ',' + str(product[1]) + '\n')
